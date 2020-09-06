b7972369609bf989718fe015f539cab018502e43
Alexander Malashkevich <62018835+MrNeelixs@users.noreply.github.com>
2020-09-06 12:25:26 +0300

    Пока без Main

--- src/main/java/com/company/Arrays.java
+++ src/main/java/com/company/Arrays.java
@@ -2,14 +2,23 @@ package com.company;
 
 /**
  * Найти среднее арифметическое всех элементов массива
- *
+ * <p>
  * Например:
- *
+ * <p>
  * Дано: [1,3,5,8]
  * Результат: 4
  */
 public class Arrays {
     public static double findAverage(int[] array) {
-        return 0;
+
+        double average = 0;
+        if (array.length > 0) {
+            double sum = 0;
+            for (int j = 0; j < array.length; j++) {  // Для начала найдем сумму всех элементов массива
+                sum += array[j];
+            }
+            average = sum / array.length;    //  Находим среднее арифметическое
+        }                                    // Сумма всех элементов массива деленная на их количество
+        return average;
     }
 }
