22f3918adaa1ad4b1620cb16240df5cbdf972f30
ShinkevichAnton <shinkevichanton94@gmail.com>
2020-09-06 13:07:48 +0300

    first

--- src/main/java/com/company/Arrays.java
+++ src/main/java/com/company/Arrays.java
@@ -2,14 +2,18 @@ package com.company;
 
 /**
  * Найти среднее арифметическое всех элементов массива
- *
+ * <p>
  * Например:
- *
+ * <p>
  * Дано: [1,3,5,8]
  * Результат: 4
  */
 public class Arrays {
     public static double findAverage(int[] array) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum / array.length;
     }
 }
--- src/main/java/com/company/Strings.java
+++ src/main/java/com/company/Strings.java
@@ -6,22 +6,35 @@ package com.company;
  * Если строка состоит из одного слова - вернуть исходную строку в верхнем регистре и количество символов в ней.
  * В качестве разделителя слов выступает пробел.
  * Знаки препинания не учитывать(представим, что их нет во входной строке)
- *
+ * <p>
  * Например:
- *
+ * <p>
  * Дано: "Java is the best language in the world"
  * Результат: "language 8"
- *
+ * <p>
  * Дано: "Live without regrets"
  * Результат: "regrets 7"
- *
+ * <p>
  * Дано: "Success"
  * Результат: "SUCCESS 7"
  */
 public class Strings {
 
     public static String findLongestString(String string) {
-        return "";
+
+        String word[] = string.trim().split(" ");
+        if (word.length == 1) {
+            return string.toUpperCase() + " " + string.length();
+        }
+        String stringArray = word[0];
+        for (String s : word) {
+            if (s.length() >= stringArray.length()) {
+                stringArray = s;
+            }
+        }
+        return stringArray + " " + stringArray.length();
     }
 
 }
+
+
