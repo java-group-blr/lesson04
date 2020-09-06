2081bde8c1f1f83a7a2e690b5a7c0903941b1a0e
ShinkevichAnton <shinkevichanton94@gmail.com>
2020-09-06 13:13:26 +0300

    second

--- src/main/java/com/company/Sort.java
+++ src/main/java/com/company/Sort.java
@@ -15,7 +15,7 @@ public class Sort {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
                 if (array[j + 1] > array[j]) {
-                    int temp = array[j + 1];
+                    int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
