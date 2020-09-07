a083ad2410e54012c0abbd4f4371f49ae10d7d1d
Angela-h-89 <angela-h-89@yandex.by>
2020-09-07 20:33:03 +0300

    Homework_4_Angela

--- src/main/java/com/company/Sort.java
+++ src/main/java/com/company/Sort.java
@@ -1,17 +1,26 @@
-package com.company;
-
-/**
- * Дан целочисленный массив. Отсортировать его по убыванию и поменять местами первый и последний элемент.
- *
- * Например:
- *
- * Дано: [1,23,42,6,8,3]
- * Результат: [1,23,8,6,3,42]
- */
+//Дан целочисленный массив. Отсортировать его по убыванию и поменять местами первый и последний элемент.
+//Например:
+//Дано: [1,23,42,6,8,3]
+//Результат: [1,23,8,6,3,42]
 public class Sort {
+    public static void sorting_numbers(int[] array) {
+        int[] arr = {1,23,42,6,8,3};
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j + 1] > array[j]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        int temp = array[0];
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
 
-    public static int[] sortArray(int[] array) {
-        return new int[0];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
-
 }
