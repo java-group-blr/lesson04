75ec336045259a4c0c92ddeff037b45d6434f45e
Angela-h-89 <angela-h-89@yandex.by>
2020-09-08 19:12:58 +0300

    HW_4_Angela

--- src/main/java/com/company/Arrays.java
+++ src/main/java/com/company/Arrays.java
@@ -1,15 +1,15 @@
-package com.company;
-
-/**
- * Найти среднее арифметическое всех элементов массива
- *
- * Например:
- *
- * Дано: [1,3,5,8]
- * Результат: 4
- */
+//Найти среднее арифметическое всех элементов массива
+//
+//Дано: [1,3,5,8]
+//Результат: 4
+//
 public class Arrays {
-    public static double findAverage(int[] array) {
-        return 0;
+    public static void average_of_numbers(int[] array) {
+        int[] arr = {1,3,5,8};
+        int sum_of_numbers = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum_of_numbers += array[i];
+        }
+        System.out.println(sum_of_numbers /array.length);
     }
 }
