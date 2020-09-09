861ac2b0259856eeb933892fba656c7311786190
Angela-h-89 <angela-h-89@yandex.by>
2020-09-09 14:08:21 +0300

    Angela_4_HW

--- src/main/java/com/company/Sort.java
+++ src/main/java/com/company/Sort.java
@@ -12,6 +12,23 @@ public class Sort {
 
     public static int[] sortArray(int[] array) {
         return new int[0];
-    }
+        int[] arr = {1,23,42,6,8,3};
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j + 1] > array[j]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        int temp = array[0];
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
 
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
