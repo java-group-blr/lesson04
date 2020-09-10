dd4bbd54ac891657b91d76d192270b4cb000b8e0
Angela-h-89 <angela-h-89@yandex.by>
2020-09-10 12:48:08 +0300

    angela

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,28 +2,36 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,3,5,8};
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        System.out.println("ЗАДАНИЕ 1");
+        int[] arr = {1, 3, 5, 8};
+        for (int i: arr){
+        //for (int i = 0; i < arr.length; i++) {
+            System.out.println(i);
         }
+        Arrays arrays = new Arrays();
+        double result = Arrays.findAverage(arr);
+        System.out.println("Среднее арифметическое всех элементов массива: " + result);
 
-        Arrays arrays=new Arrays();
-        double result=Arrays.findAverage(arr);
-        System.out.println("Среднее арифметическое всех элементов массива: "+result);
-
-        int[] arr2 = {1,23,42,6,8,3};
-
-        for (int i = 0; i < arr2.length; i++) {
-            System.out.println(arr2[i]);
+        System.out.println("ЗАДАНИЕ 2");
+        int[] arr2 = {1, 23, 42, 6, 8, 3};
+        for (int i: arr2){
+        //for (int i = 0; i < arr2.length; i++) {
+            System.out.println(i);
         }
-
-        Sort sort=new Sort();
-        int arr_result[]=Sort.sortArray(arr2);
-
+        Sort sort = new Sort();
+        int arr_result[] = Sort.sortArray(arr2);
         System.out.println("Отсортированный массив: ");
-        for (int i = 0; i < arr_result.length; i++) {
-            System.out.println(arr_result[i]);
+        for (int i: arr_result){
+        //for (int i = 0; i < arr_result.length; i++) {
+            System.out.println(i);
         }
+
+        System.out.println("ЗАДАНИЕ 3");
+        String arr3 = "Live without regrets";
+        System.out.println(arr3);
+        Strings strings = new Strings();
+        String arr_word = Strings.findLongestString(arr3);
+        System.out.println("Cамое длинное слово и количество символов в нем: ");
+        System.out.println(arr_word);
     }
 }
