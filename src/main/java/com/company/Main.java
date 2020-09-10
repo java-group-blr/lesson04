6b0add81b8c08694ad1de8e4add3f506f72415cb
Angela-h-89 <angela-h-89@yandex.by>
2020-09-10 13:17:38 +0300

    angela

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,28 +6,30 @@ public class Main {
         int[] arr = {1, 3, 5, 8};
         for (int i: arr){
         //for (int i = 0; i < arr.length; i++) {
-            System.out.println(i);
+            System.out.print(i+" ");
         }
         Arrays arrays = new Arrays();
         double result = Arrays.findAverage(arr);
-        System.out.println("Среднее арифметическое всех элементов массива: " + result);
+        System.out.println("\nСреднее арифметическое всех элементов массива: " + result);
 
+        System.out.println();
         System.out.println("ЗАДАНИЕ 2");
         int[] arr2 = {1, 23, 42, 6, 8, 3};
         for (int i: arr2){
         //for (int i = 0; i < arr2.length; i++) {
-            System.out.println(i);
+            System.out.print(i+" ");
         }
         Sort sort = new Sort();
         int arr_result[] = Sort.sortArray(arr2);
-        System.out.println("Отсортированный массив: ");
+        System.out.println("\nОтсортированный массив: ");
         for (int i: arr_result){
         //for (int i = 0; i < arr_result.length; i++) {
-            System.out.println(i);
+            System.out.print(i+" ");
         }
 
-        System.out.println("ЗАДАНИЕ 3");
-        String arr3 = "Success";
+        System.out.println();
+        System.out.println("\nЗАДАНИЕ 3");
+        String arr3 = "Live without regrets";
         System.out.println(arr3);
         Strings strings = new Strings();
         String arr_word = Strings.findLongestString(arr3);
